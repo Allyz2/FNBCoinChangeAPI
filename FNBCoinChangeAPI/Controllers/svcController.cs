8c335e6ac709f5a726d6e7b2df31292a5f96f425
Allyz2 <115589851+Allyz2@users.noreply.github.com>
2022-10-12 00:11:31 +0200

    Add project files.

---
+++ FNBCoinChangeAPI/Controllers/svcController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FNBCoinChangeAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class svcController : ControllerBase
+    {
+        // GET: api/<svcController>
+        [HttpGet]
+        public int Get([FromQuery(Name ="Coins")]int[] coins, int amount)
+        {
+            Array.Sort(coins);
+            // create an array to hold the x amount of coints we can have...
+            //We need amount + 1 slots since this is zero based
+            int[] dp = new int[amount + 1];
+            // since everything is is invalid, we need to fill in the array with every thing to try 
+            // sort out the subproblems
+            Array.Fill(dp, amount + 1);
+            // what is the minimum amount of coins we ned to make o couns
+            // the answer is 0
+            dp[0] = 0;
+
+            // now to determine the fewest number of coins to make the amount....
+            // we need to loop through the  amount and test each coun as we move along
+            for (int i = 0; i <= amount; i++)
+            {
+                for (int j = 0; j < coins.Length; j++) // coints loop
+                {
+                    //check if the current coin is less than the current amount
+                    if (coins[j] <= i)
+                    {
+                        // get the best minimum number of coins for the current Payout Amount
+                        dp[i] = Math.Min(dp[i], 1 + dp[i - coins[j]]);
+                    }
+                    else
+                    {
+                        // no point of going through bigger and bigger coins
+                        break;
+                    }
+                }
+            }
+
+            return dp[amount] > amount ? -1 : dp[amount];
+        }
+
+
+
+
+    }
+}
---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        string baseUrl = "http://localhost/";
+
+
+        public ActionResult GeChange()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public JsonResult GePayoutChange(FormCollection frm)
+        {
+            var _v1 = int.Parse(frm["v1"]);
+            var _v2 = int.Parse(frm["v2"]);
+            var _v3 = int.Parse(frm["v3"]);
+            var _Amount = int.Parse(frm["Amount"]);
+
+            int[] _arraay = new int[] { _v1,_v2,_v3};
+
+            string _testres = "" ;
+
+
+           // string ApiMethodNameParam = "svc?Coins=" +_arraay  + "&amount=" + _Amount;
+
+            //Try call the coin change  api hosted locally on iis
+             string ApiMethodNameParam ="svc?Coins=" + _v1 + "&Coins=" + _v2 + "&Coins="+ _v3 + "&amount=" + _Amount;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseUrl + "api/");
+                //HTTP GET
+                var responseTask = client.GetAsync(ApiMethodNameParam);
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    // var readTask = result.Content();
+                     readTask.Wait();
+                    _testres = readTask.Result;
+                }
+
+            }
+            //return string json result
+            return Json(_testres, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+
+
+        public ActionResult Index()
+        {
+            //redirect user to the change coin page
+            return RedirectToAction("GeChange", "Home");
+            //return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
